662c3dc3e1368f91d46cdfc18d2ed648ba415bbc
PriNT2357 <PriNT2357@users.noreply.github.com>
2021-12-27 21:08:16 -0600

    Update buyServer
    - Add command to transfer files
    - Add command to run the dynamic keeprunning script

--- Scripts/buyServer.js
+++ Scripts/buyServer.js
@@ -1,9 +1,7 @@
-Attempt to purchase a server
-
 /** @param {NS} ns **/
 export async function main(ns) {
 	if (ns.args.length !== 2 || typeof ns.args[0] !== "string" || typeof ns.args[1] !== "number") {
-		ns.tprint("Usage: >run buyserver.js _hostname_ _ram_");
+		ns.tprint("Usage: > run buyserver.js HOSTNAME RAM");
 		ns.exit;
 	}
 
@@ -14,5 +12,13 @@ export async function main(ns) {
 		ns.tprint("Unable to purchase server");
 	} else {
 		ns.tprint("Purchased new server " + hostname + " with " + ram + "GB memory");
+		// Allow time for the hostname to populate through dns (wait for the game to set it up)
+		await ns.sleep(1500);
+		ns.run("/simple/xfer.js", 1, hostname);
+		// This will trigger the dynamic scripts to start running
+		var pid = ns.exec("/dynamic/keeprunning.js", hostname, 1);
+		if (pid === 0) {
+			ns.tprint("Unable to execute the keeprunning script on " + hostname);
+		}
 	}
 }
