4d99edec815a885fa086cb653b48aac06315a646
PriNT2357 <PriNT2357@users.noreply.github.com>
2022-01-06 12:33:28 -0600

    Update transfer script
    - Expands default folders that are transferred
    - Allow transferring to multiple destinations at once

--- Scripts/xfer.js
+++ Scripts/xfer.js
@@ -1,9 +1,18 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	var dest = ns.args[0];
-	var files = ["/simple/hackit.js", "/simple/weaken.js", "/simple/growit.js"];
 	var source = "home";
-	files = files.concat(ns.ls(source, "/dynamic/"));
-	files = files.concat(ns.ls(source, "/public/"));
-	await ns.scp(files, source, dest);
+	var files = ["/public/monitor.js"];
+	files = addfolder(ns, source, "/simple/", files);
+	files = addfolder(ns, source, "/dynamic/", files);
+	for (let dest of ns.args) {
+		//var dest = ns.args[0];
+		// ns.tprint("xfer: " + files);
+		await ns.scp(files, source, dest);
+	}
+	
+}
+function addfolder(ns, source, folder, files) {
+	var otherfiles = ns.ls(source, folder);
+	files = files.concat(otherfiles);
+	return files;
 }
