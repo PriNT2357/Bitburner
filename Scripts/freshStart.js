ec4f3aed55204bf89d4201a3aad0e20159cad23d
PriNT2357 <PriNT2357@users.noreply.github.com>
2022-01-10 19:43:48 -0600

    Added script for fresh starts
    - Needs lots of ram to work
    - More useful after several soft resets

---
+++ Scripts/freshStart.js
@@ -0,0 +1,28 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	// purchase tools
+	ns.purchaseTor();
+	const programs = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"];
+	var progs = 0;
+	var firstLoop = true;
+	while (progs < programs.length) {
+		if (firstLoop) {
+			firstLoop = false;
+			ns.run("/simple/deployLoop.js");
+			ns.run("/simple/installBackdoors.js");
+			ns.run("/simple/buyServers.js");
+		}
+		for (let prog of programs) {
+			if (!ns.fileExists(prog, "home")) {
+				ns.purchaseProgram(prog);
+				if (!ns.getPlayer().isWorking) {
+					ns.createProgram(prog);
+				}
+			} else {
+				progs++;
+			}
+		}
+
+		await ns.sleep(60000);
+	}
+}
