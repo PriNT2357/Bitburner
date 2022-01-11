cbcf59ab93761d7b5d41ecacb5223445fd999de6
PriNT2357 <PriNT2357@users.noreply.github.com>
2022-01-11 09:29:58 -0600

    Expand listed augmentation info
    - Include price and reputation requirement

--- Scripts/availableAugs.js
+++ Scripts/availableAugs.js
@@ -7,19 +7,41 @@ export async function main(ns) {
 		includePurchased = true;
 	}
 	var factions = ns.getPlayer().factions;
-	var installedAugs = ns.getOwnedAugmentations(includePurchased);
+	var installedAugs = ns.getOwnedAugmentations(true);
 	for (let faction of factions) {
-		ns.tprint(faction);
+		var rep = ns.getFactionRep(faction);
+		ns.tprintf("%s - (%s rep)", faction, formatNumber(rep));
 		var availableAugs = ns.getAugmentationsFromFaction(faction);
 		for (let aug of availableAugs) {
+			var price = ns.getAugmentationPrice(aug);
+			var repreq = ns.getAugmentationRepReq(aug);
+			var output = "   %3s %-42s %7s %7s";
+			var haveit = false;
 			if (installedAugs.indexOf(aug) > -1) {
+				haveit = true;
+			}
+
+			if (haveit) {
 				if (includePurchased) {
-					ns.tprint(" ☑ " + aug);
+					ns.tprintf(output, (haveit ? " ☑ " : " ☐ "), aug, formatNumber(price), formatNumber(repreq));
 				}
 			} else {
-				ns.tprint(" ☐ " + aug);
+				ns.tprintf(output, (haveit ? " ☑ " : " ☐ "), aug, formatNumber(price), formatNumber(repreq));
 			}
 		}
 
 	}
+}
+
+function formatNumber(n) {
+	const div = 1000;
+	var m = n * div; // account for first run of do{}
+	var labels = ["", "k", "m", "b", "t"];
+	var label = "";
+	do {
+		m = Number(m / div).toFixed(1); //convert to closest label
+		label = labels.shift();
+	} while (m >= div && labels.length !== 0)
+
+	return m + label;
 }
